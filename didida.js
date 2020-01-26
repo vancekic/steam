75fa02531b5aaddf4aeff43a808eaf95f01e698f
Stephane Assadourian <vancekic@gmail.com>
2020-01-26 19:05:18 +0100

    successfully saves the search into a csv file named export.csv

--- didida.js
+++ didida.js
@@ -1,8 +1,23 @@
 var steam = require('steam-searcher')
-
+var converter = require('json-2-csv')
+const fs = require ('fs')
 
 steam.find({ search: 'Total War: Three Kingdoms' }, function (err, game) {
     if (err) console.log(err);
     //game is the data as a JSON.
     console.log('Current price = ' + game.price_overview.final_formatted)
+
+    console.log('Converting JSON to CSV...')
+    var csv = converter.json2csv(game, function (err, product) {
+        if (err) throw err;
+        console.log('conversion done!')
+        console.log('saving to file export.csv...')
+        fs.open('./export.csv', 'w', (err, fd) => {
+            if (err) throw err;
+            fs.write(fd, product, function(err) {
+                if (err) throw err;
+                console.log('File has been saved successfully!')
+            })
+        })
+    })
 });
