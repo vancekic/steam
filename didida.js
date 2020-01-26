4926d384e563c4cf1cbb60e5dd91ace9725fe46a
Stephane Assadourian <vancekic@gmail.com>
2020-01-26 21:42:02 +0100

    aggregating several results in the csv file + refactored code + added skeleton for findGenre function

--- didida.js
+++ didida.js
@@ -2,16 +2,14 @@ var steam = require('steam-searcher')
 var converter = require('json-2-csv')
 const fs = require ('fs')
 
-steam.find({ search: 'Total War: Three Kingdoms' }, function (err, game) {
+steam.find({ search: 'Tomb Raider' }, function (err, game) {
     if (err) console.log(err);
     //game is the data as a JSON.
-    console.log('Current price = ' + game.price_overview.final_formatted)
-
     console.log('Converting JSON to CSV...')
     var csv = converter.json2csv(game, function (err, product) {
         if (err) throw err;
         console.log('conversion done!')
-        console.log('saving to file export.csv...')
+        console.log('Now saving to file export.csv...')
         fs.open('./export.csv', 'w', (err, fd) => {
             if (err) throw err;
             fs.write(fd, product, function(err) {
@@ -21,3 +19,23 @@ steam.find({ search: 'Total War: Three Kingdoms' }, function (err, game) {
         })
     })
 });
+
+/*
+steam.findGenre({ search: 'Strategy' }, function (err, game) {
+    if (err) console.log(err);
+    //game is the data as a JSON.
+    console.log('Converting JSON to CSV...')
+    var csv = converter.json2csv(game, function (err, product) {
+        if (err) throw err;
+        console.log('conversion done!')
+        console.log('Now saving to file export.csv...')
+        fs.open('./export.csv', 'w', (err, fd) => {
+            if (err) throw err;
+            fs.write(fd, product, function(err) {
+                if (err) throw err;
+                console.log('File has been saved successfully!')
+            })
+        })
+    })
+});
+*/
