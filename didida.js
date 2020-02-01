9e5e582a8f287dfc409502a90b2ec75078c013fc
Stephane Assadourian <vancekic@gmail.com>
2020-02-01 20:30:52 +0100

    wip refactoring to use reg exp from html response from steam

--- didida.js
+++ didida.js
@@ -2,7 +2,8 @@ var steam = require('steam-searcher')
 var converter = require('json-2-csv')
 const fs = require ('fs')
 
-steam.find({ search: 'Creed' }, function (err, game) {
+
+steam.find(function (err, game) {
     if (err) return console.log(err);
     //game is the data as a JSON.
     console.log('Converting JSON to CSV...')
@@ -21,6 +22,24 @@ steam.find({ search: 'Creed' }, function (err, game) {
 });
 
 /*
+steam.findGame({ search: 'Creed' }, function (err, game) {
+    if (err) return console.log(err);
+    //game is the data as a JSON.
+    console.log('Converting JSON to CSV...')
+    var csv = converter.json2csv(game, function (err, product) {
+        if (err) throw err;
+        console.log('conversion done!')
+        console.log('Now saving to file export.csv...')
+        fs.open('./export.csv', 'w', (err, fd) => {
+            if (err) throw err;
+            fs.write(fd, product, function(err) {
+                if (err) throw err;
+                console.log('File has been saved successfully!')
+            })
+        })
+    })
+});
+
 steam.findGenre({ search: 'Strategy' }, function (err, game) {
     if (err) return console.log(err)
     //game is the data as a JSON.
